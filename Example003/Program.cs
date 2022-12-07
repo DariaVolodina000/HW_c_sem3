07a7054dd283094ba6c79fce2ba7509d4121914e
Daria <dashavolodina@gmail.com>
2022-12-07 11:51:22 +0800

    Рабочая версия. Кубы чисел

---
+++ Example003/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void Kub(int kub, int index)
+{
+    kub = (index * index * index);
+    Console.Write($"{kub}, ");
+}
+
+try
+{
+Console.WriteLine("Напишите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int k = 1;
+
+while (count <= num)
+{
+    Kub(k, count);
+    count++;
+    k++;
+}
+}
+catch 
+{
+    Console.WriteLine("Введите число цифрами ");
+}
+
